c24cd6536e69cbe2fc7369b6b4705a40df7b9a5f
irreg <irreg@melancholy.orz.hm>
2025-08-30 15:53:34 +0900

    refactor

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import copy
 import json
 import os
-from typing import Any
+from typing import Any, AsyncIterator
 
 import httpx
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse, StreamingResponse
 
-from parser import (
+from parser_control import (
     generate_tool_schemas,
     modify_tool_call_to_xml_message,
     modify_tool_calls_to_xml_messages,
@@ -38,6 +38,68 @@ def process_request(req: dict[str, Any]) -> dict[str, Any]:
     return req
 
 
+async def handle_stream(r: httpx.Response) -> AsyncIterator[str]:
+    if r.is_error:
+        await r.aread()
+        yield f"data: {r.text}\n\n"
+        yield "data: [DONE]\n\n"
+        return
+    buffer = ""
+    last_chunk = None
+    role = None
+    choice_index = 0
+    tool_call_index = 0
+    tool_call_id = ""
+    tool_name = ""
+    async for line in r.aiter_lines():
+        if not line.startswith("data: "):
+            continue
+
+        def create_tool_call():
+            nonlocal buffer, tool_name, tool_call_id
+            modified_data = modify_tool_call_to_xml_message(
+                tool_name, buffer, tool_call_id
+            )
+            last_chunk["choices"][0]["delta"]["content"] = modified_data
+            buffer = ""
+            tool_name = ""
+            tool_call_id = ""
+            return f"data: {json.dumps(last_chunk, ensure_ascii=False)}\n\n"
+
+        if line.strip() == "data: [DONE]":
+            if buffer:
+                yield create_tool_call()
+            yield line + "\n\n"
+            continue
+        data = json.loads(line[6:])
+        choice = (data.get("choices") or [{}])[0]
+        delta = choice.get("delta", {})
+        if (
+            not delta
+            or not delta.get("tool_calls")
+            or delta.get("role", role) != role
+            or choice.get("index") != choice_index
+        ) and buffer:
+            yield create_tool_call()
+        role = delta.get("role", role)
+        choice_index = choice.get("index", choice_index)
+        if role == "assistant":
+            tool_call = (delta.get("tool_calls") or [{}])[0]
+            if tool_call.get("index") != tool_call_index and buffer:
+                yield create_tool_call()
+            if tool_call:
+                tool_name += tool_call.get("function").get("name", "")
+                buffer += tool_call.get("function").get("arguments", "")
+                tool_call_id += tool_call.get("id", "")
+                tool_call_index = tool_call.get("index", tool_call_index)
+                last_chunk = data
+        if data.get("finish_reason") and buffer:
+            yield create_tool_call()
+        if data.get("finish_reason") == "tool_calls":
+            data["finish_reason"] = "stop"
+        yield "data: " + json.dumps(data, ensure_ascii=False) + "\n\n"
+
+
 @app.post("/v1/chat/completions")
 async def create_completion(request: Request):
     modified_req = process_request(await request.json())
@@ -50,7 +112,7 @@ async def create_completion(request: Request):
     stream = modified_req.get("stream")
     if stream:
 
-        async def event_stream():
+        async def event_stream() -> AsyncIterator[str]:
             async with httpx.AsyncClient(timeout=None) as client:
                 async with client.stream(
                     "POST",
@@ -59,67 +121,8 @@ async def create_completion(request: Request):
                     headers=headers,
                     params=request.query_params,
                 ) as r:
-                    if r.is_error:
-                        await r.aread()
-                        yield f"data: {r.text}\n\n"
-                        yield "data: [DONE]\n\n"
-                        return
-                    buffer = ""
-                    last_chunk = None
-                    role = None
-                    choice_index = 0
-                    tool_call_index = 0
-                    tool_call_id = ""
-                    tool_name = ""
-                    async for line in r.aiter_lines():
-                        if not line.startswith("data: "):
-                            continue
-
-                        def create_tool_call():
-                            nonlocal buffer, tool_name, tool_call_id
-                            modified_data = modify_tool_call_to_xml_message(
-                                tool_name, buffer, tool_call_id
-                            )
-                            last_chunk["choices"][0]["delta"]["content"] = modified_data
-                            buffer = ""
-                            tool_name = ""
-                            tool_call_id = ""
-                            return f"data: {json.dumps(last_chunk, ensure_ascii=False)}\n\n"
-
-                        if line.strip() == "data: [DONE]":
-                            if buffer:
-                                yield create_tool_call()
-                            yield line + "\n\n"
-                            continue
-                        data = json.loads(line[6:])
-                        choice = (data.get("choices") or [{}])[0]
-                        delta = choice.get("delta", {})
-                        if (
-                            not delta
-                            or not delta.get("tool_calls")
-                            or delta.get("role", role) != role
-                            or choice.get("index") != choice_index
-                        ) and buffer:
-                            yield create_tool_call()
-                        role = delta.get("role", role)
-                        choice_index = choice.get("index", choice_index)
-                        if role == "assistant":
-                            tool_call = (delta.get("tool_calls") or [{}])[0]
-                            if tool_call.get("index") != tool_call_index and buffer:
-                                yield create_tool_call()
-                            if tool_call:
-                                tool_name += tool_call.get("function").get("name", "")
-                                buffer += tool_call.get("function").get("arguments", "")
-                                tool_call_id += tool_call.get("id", "")
-                                tool_call_index = tool_call.get(
-                                    "index", tool_call_index
-                                )
-                                last_chunk = data
-                        if data.get("finish_reason") and buffer:
-                            yield create_tool_call()
-                        if data.get("finish_reason") == "tool_calls":
-                            data["finish_reason"] = "stop"
-                        yield "data: " + json.dumps(data, ensure_ascii=False) + "\n\n"
+                    async for iter in handle_stream(r):
+                        yield iter
 
         return StreamingResponse(event_stream(), media_type="text/event-stream")
     else:
--- parser.py
+++ parser.py
@@ -1,4 +1,3 @@
-import copy
 import hashlib
 import json
 import re
@@ -415,28 +414,15 @@ def convert_xml_example_to_json(
     )
 
 
-def generate_tool_schemas(doc: str) -> tuple[list[JsonObj], str]:
-    tools_md = extract_tools_section(doc)
-    tools = parse_tools_section(tools_md)
-    tools_schemas = []
-    new_doc = doc
-
-    for t in tools:
-        schema = build_tool_schema(t)
-        tools_schemas.append(schema)
-        # Convert each XML usage into a JSON call sample
-        for x in t.xml_samples:
-            json_example = convert_xml_example_to_json(t.name, x, tools_schemas)
-            new_doc = new_doc.replace(x, json_example)
-
+def remove_duplicated_section_from_doc(doc: str) -> str:
     # Remove duplicated sections from the doc
     new_doc = re.sub(
         r"^(?:\*\*)?(Required |Optional )?(Description|Parameter)s?:(?:\*\*)?\s*([\s\S]*?)(?=^(\*\*)?((Required |Optional ) ?Parameters?:|##?\s+|Usages?:|(Usage )?Examples?(\b[\w ]+)?:|\Z)(\*\*)?)",
         "",
-        new_doc,
+        doc,
         flags=re.MULTILINE,
     )
-    return tools_schemas, new_doc
+    return new_doc
 
 
 def convert_obj_to_xml_with_id(
@@ -473,37 +459,6 @@ def convert_obj_to_xml_with_id(
     return xml_str
 
 
-def modify_tool_calls_to_xml_messages(
-    messages: list[dict[str, Any]],
-) -> list[dict[str, Any]]:
-    messages = copy.deepcopy(messages)
-    for choice in messages.get("choices", []):
-        if choice["message"]["role"] == "assistant" and choice["message"].get(
-            "tool_calls"
-        ):
-            xml_parts = []
-            for tool_call in choice["message"]["tool_calls"]:
-                xml_parts.append(
-                    convert_obj_to_xml_with_id(
-                        json.loads(tool_call["function"]["arguments"]),
-                        root_name=tool_call["function"]["name"],
-                        id=tool_call["id"],
-                    )
-                )
-            choice["message"]["content"] = (
-                choice["message"].get("content") or ""
-            ) + "\n".join(xml_parts)
-            if choice["finish_reason"] == "tool_calls":
-                choice["finish_reason"] = "stop"
-    return messages
-
-
-def modify_tool_call_to_xml_message(
-    name: str, tool_call: str, id: str
-) -> dict[str, Any]:
-    return convert_obj_to_xml_with_id(json.loads(tool_call), root_name=name, id=id)
-
-
 def convert_xml_to_obj_exclude_id(
     xml_string: str, tool_schemas: list[JsonObj]
 ) -> tuple[str, JsonObj, str]:
@@ -522,61 +477,3 @@ def convert_xml_to_obj_exclude_id(
         id_value = hashlib.md5(xml_string.encode()).hexdigest()
 
     return root.tag, convert_xml_element_to_obj(root, tool_schemas), id_value
-
-
-def modify_xml_messages_to_tool_calls(
-    messages: list[dict[str, Any]], tool_schemas: list[JsonObj]
-) -> list[dict[str, Any]]:
-    messages = copy.deepcopy(messages)
-    last_id_value: list[str] | None = []
-    last_tool_name: list[str] | None = []
-    for message in messages:
-        if message["role"] == "assistant":
-            if message["content"] and isinstance(message["content"], str):
-                tool_calls = []
-                last_id_value = []
-                last_tool_name = []
-                # Parse XML content
-                xml_tool_calls = extract_xml_blocks_for_tool(
-                    message["content"],
-                    [s["function"]["name"] for s in tool_schemas],
-                )
-                for xml in xml_tool_calls:
-                    try:
-                        name, json_dict, id_value = convert_xml_to_obj_exclude_id(
-                            xml, tool_schemas
-                        )
-                    except ET.ParseError:
-                        continue  # Skip if content is not valid XML
-                    tool_call = {
-                        "type": "function",
-                        "id": id_value,
-                        "function": {
-                            "name": name,
-                            "arguments": json.dumps(json_dict, ensure_ascii=False),
-                        },
-                    }
-                    tool_calls.append(tool_call)
-                    last_id_value.append(id_value)
-                    last_tool_name.append(name)
-                    message["content"] = message["content"].replace(xml, "")
-                message["tool_calls"] = tool_calls
-                continue
-        if (
-            message["role"] == "user"
-            and last_id_value
-            and isinstance(message["content"], list)
-            and message["content"]
-            and (message["content"][0].get("text") or "").startswith(
-                f"[{last_tool_name[0]} "
-            )
-        ):
-            # If user message has tool calls, append last tool call
-            message["role"] = "tool"
-            message["tool_call_id"] = last_id_value[0]
-            last_id_value = last_id_value[1:]
-            last_tool_name = last_tool_name[1:]
-            continue
-        last_id_value = []
-        last_tool_name = []
-    return messages
